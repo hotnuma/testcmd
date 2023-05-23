a4c959dc4843b855b3cbd10ff5129d6802342ca7
hotnuma <hotnuma@gmail.com>
2023-05-23 13:49:01 +0200

    dirstats : count number of part in path

--- tools/sys_dirstat.c
+++ tools/sys_dirstat.c
@@ -3,22 +3,31 @@
 
 // parse all subdirectories and get statistics
 
+// number of files in directory
 CString *_count_path = NULL;
 int _count_result = 0;
 
+// longest path
 CString *_long_path = NULL;
 int _long_result = 0;
+
+CString *_npart_path = NULL;
+int _npart_result = 0;
+
+int part_count(const char *path);
 
 void init()
 {
     _count_path = cstr_new_size(256);
     _long_path = cstr_new_size(256);
+    _npart_path = cstr_new_size(256);
 }
 
 void release()
 {
     cstr_free(_count_path);
     cstr_free(_long_path);
+    cstr_free(_npart_path);
 }
 
 void parse(const char *dirpath)
@@ -35,10 +44,19 @@ void parse(const char *dirpath)
         ++count;
 
         int length = cstr_size(filepath);
+
         if (length > _long_result)
         {
             cstr_copy_len(_long_path, c_str(filepath), length);
             _long_result = length;
+        }
+
+        int npart = part_count(c_str(filepath));
+
+        if (npart > _npart_result)
+        {
+            cstr_copy_len(_npart_path, c_str(filepath), length);
+            _npart_result = npart;
         }
     }
 
@@ -48,6 +66,20 @@ void parse(const char *dirpath)
         cstr_copy(_count_path, dirpath);
         _count_result = count;
     }
+}
+
+int part_count(const char *path)
+{
+    int count = 0;
+    const char *p = path;
+
+    while ((p = strchr(p, '/')) != NULL)
+    {
+        ++count;
+        ++p;
+    }
+
+    return count;
 }
 
 int main()
@@ -79,6 +111,10 @@ int main()
     print("--------------------------------------------------");
     print("long_path : %s", c_str(_long_path));
     print("long_result : %d", _long_result);
+
+    print("--------------------------------------------------");
+    print("npart_path : %s", c_str(_npart_path));
+    print("npart_result : %d", _npart_result);
 
     print("--------------------------------------------------");
     print("numdirs : %d", numdirs);
