afccbf2ed84d93d4119e7073971069a2665062db
hotnuma <hotnuma@gmail.com>
2023-06-19 05:31:26 +0200

    cleanup

--- 0Temp.c
+++ 0Temp.c
@@ -1,20 +1,5 @@
 #if 0
 
-CList* clist_new_args(int argc, char **argv)
-{
-    if (argc < 1)
-        return NULL;
-
-    CList *list = clist_new(argc, (CDeleteFunc) free);
-
-    for (int i = 0; i < argc; ++i)
-    {
-        clist_append(list, strdup(argv[i]));
-    }
-
-    return list;
-}
-
 #endif
 
 
