475888c5ac30ea495c23c2f9f42e262d307e9eb0
hotnuma <hotnuma@gmail.com>
2023-11-21 11:47:01 +0100

    try locale strcasecmp

--- 0Temp.c
+++ 0Temp.c
@@ -1,5 +1,27 @@
 #if 0
 
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <ctype.h>
+#include <print.h>
+
+
+int main()
+{
+    //setlocale(LC_COLLATE, "fr_FR.UTF-8");
+    setlocale(LC_COLLATE, "");
+    //setlocale(LC_ALL, "");
+
+    print("cmp = %d", strcasecmp("b", "à"));
+
+    //print("char = %c", (char) tolower('à'));
+
+    return EXIT_SUCCESS;
+}
+
+
+
 #endif
 
 
